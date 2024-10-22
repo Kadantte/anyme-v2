423c1180fd4444dd3a7b04a96d1122749c2236bd
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-10-22 17:11:50 +0700

    hide devtools

--- src/lib/ReactQueryProviders.tsx
+++ src/lib/ReactQueryProviders.tsx
@@ -10,7 +10,7 @@ const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools />
+      {/* <ReactQueryDevtools /> */}
     </QueryClientProvider>
   );
 };
