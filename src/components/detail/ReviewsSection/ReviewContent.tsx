a1ccdac0614252fdd479d64ebf979beaa943a48f
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-10-27 16:58:38 +0700

    feat: add detail review

--- src/components/detail/ReviewsSection/ReviewContent.tsx
+++ src/components/detail/ReviewsSection/ReviewContent.tsx
@@ -1,7 +1,16 @@
 import Image from 'next/image';
-import { FaStar } from 'react-icons/fa6';
+import { FaReadme, FaStar } from 'react-icons/fa6';
 import { ImQuotesLeft } from 'react-icons/im';
 import { TiWarning } from 'react-icons/ti';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import Link from 'next/link';
 
 export default function ReviewContent({ reviewsList, scrollRef }: any) {
   return (
@@ -13,11 +22,39 @@ export default function ReviewContent({ reviewsList, scrollRef }: any) {
         ? reviewsList.data.map((review: any) => (
             <div
               key={review.mal_id}
-              className="h-[400px] md:h-[250px] lg:h-[280px] xl:h-[320px] w-[250px] md:w-[350px] lg:w-[380px] xl:w-[450px] rounded-xl flex flex-col justify-between px-4 md:px-6 lg:px-8 py-2 md:py-4 lg:py-6 flex-shrink-0 bg-gradient-to-tl from-neutral-900 via-neutral-900 to-neutral-800 snap-end"
+              className="relative overflow-hidden h-[400px] md:h-[250px] lg:h-[280px] xl:h-[320px] w-[250px] md:w-[350px] lg:w-[380px] xl:w-[450px] rounded-xl flex flex-col justify-between px-4 md:px-6 lg:px-8 py-2 md:py-4 lg:py-6 flex-shrink-0 bg-gradient-to-tl from-neutral-900 via-neutral-900 to-neutral-800 snap-end group cursor-pointer"
             >
+              <Dialog>
+                <DialogTrigger className="absolute w-full h-full inset-0 flex flex-col justify-center items-center bg-neutral-200/0 bg-clip-padding backdrop-filter backdrop-blur-sm backdrop-saturate-100 backdrop-contrast-100 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out">
+                  <FaReadme className="size-20 md:size-22 lg:size-24 text-neutral-50" />
+                  <span className="text-[1.5rem] md:text-[1.6rem] lg:text-[1.8rem] font-bold text-neutral-50">
+                    Read More
+                  </span>
+                </DialogTrigger>
+                <DialogContent className="w-[80%] h-[80%] rounded-xl overflow-auto no-scrollbar bg-gradient-to-tl from-neutral-900 via-neutral-800 to-neutral-700 border-none px-4 py-2 md:px-5 md:py-3 lg:px-6 lg:py-4">
+                  <DialogHeader>
+                    <DialogTitle className="text-neutral-50 text-start font-semibold text-[1rem] md:text-[1.1rem] lg:text-[1.2rem]">
+                      <Link
+                        href={review.user.url}
+                        target="_blank"
+                        className="hover:text-violet-500"
+                      >
+                        {review.user.username} &nbsp;
+                      </Link>
+                      <span>&#183; &nbsp;</span>
+                      <span>&#9733; {review.score}</span>
+                    </DialogTitle>
+                    <DialogDescription className="text-neutral-200 text-start font-normal text-[0.8rem] md:text-[0.95rem] lg:text-[1rem]">
+                      {review.review}
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
               <ImQuotesLeft className="text-neutral-500 size-8 md:size-9 lg:size-10" />
               <p className="text-neutral-300 text-[0.8rem] md:text-[0.95rem] lg:text-[1rem] xl:text-[1.05rem]">
-                {review.review.substring(0, 100)}...
+                {review.review.length > 100
+                  ? review.review.substring(0, 100) + '...'
+                  : review.review}
               </p>
               <div className="w-full flex justify-between items-center text-neutral-300">
                 <div className="flex items-center gap-x-2">
