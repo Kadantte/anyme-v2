432d76f645f3fd8f7034977305d67f0984ea0641
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-10-31 15:31:03 +0700

    feat: styling footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,67 @@
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa6';
+
 export default function Footer() {
   return (
-    <footer className="bg-neutral-950">
-      <div className="wrapper py-8 md:py-12">Footer</div>
+    <footer className="bg-neutral-900">
+      <div className="wrapper pt-8 md:pt-8">
+        <div className="flex justify-between items-start w-full pb-8">
+          <div className="max-w-[400px] bg-neutral-800 p-4 rounded-lg">
+            <h1 className="text-[1.8rem] font-bold text-neutral-50">AnyMe.</h1>
+            <span className="text-[1rem] font-bold text-violet-500 ">
+              アニメに関することなら何でも
+            </span>
+            <p className="text-[0.8rem] text-neutral-200 mt-2">
+              AnyMe is your go-to anime hub, where you can explore, discover,
+              and track your favorite shows effortlessly. Find the best
+              recommendations and explore new anime by genre, popularity, or
+              trends.
+            </p>
+            <div className="flex items-center gap-x-4 text-neutral-100 mt-3">
+              <FaGithub className="size-6" />
+              <FaLinkedin className="size-6" />
+              <FaInstagram className="size-6" />
+            </div>
+          </div>
+          <div className="flex flex-col gap-x-2">
+            <span className="text-[1rem] text-neutral-50 font-bold">
+              Quick Links
+            </span>
+            <span className="text-neutral-200 text-[0.9rem]">Home</span>
+            <span className="text-neutral-200 text-[0.9rem]">Bookmark</span>
+            <span className="text-neutral-200 text-[0.9rem]">Sign In</span>
+          </div>
+          <div className="flex flex-col gap-x-2">
+            <span className="text-[1rem] text-neutral-50 font-bold">
+              Top Anime
+            </span>
+            <span className="text-neutral-200 text-[0.9rem]">All Anime</span>
+            <span className="text-neutral-200 text-[0.9rem]">Top Airing</span>
+            <span className="text-neutral-200 text-[0.9rem]">Top Upcoming</span>
+            <span className="text-neutral-200 text-[0.9rem]">Top Movies</span>
+            <span className="text-neutral-200 text-[0.9rem]">Most Popular</span>
+            <span className="text-neutral-200 text-[0.9rem]">
+              Most Favorited
+            </span>
+          </div>
+          <div className="flex flex-col gap-x-2">
+            <span className="text-[1rem] text-neutral-50 font-bold">
+              Social
+            </span>
+            <span className="text-neutral-200 text-[0.9rem]">Github</span>
+            <span className="text-neutral-200 text-[0.9rem]">Linkedin</span>
+            <span className="text-neutral-200 text-[0.9rem]">Instagram</span>
+            <span className="text-neutral-200 text-[0.9rem]">Facebook</span>
+          </div>
+        </div>
+        <div className="flex items-center justify-between border-t border-neutral-700 py-4">
+          <span className="text-[1rem] text-neutral-200">
+            © 2024 AnyMe. All Rights Reserved
+          </span>
+          <span className="text-[1rem] text-neutral-200">
+            Powered by Jikan API
+          </span>
+        </div>
+      </div>
     </footer>
   );
 }
--- src/components/home/DiscoverGenreSection/index.tsx
+++ src/components/home/DiscoverGenreSection/index.tsx
@@ -20,7 +20,7 @@ export default function DiscoverGenreSection() {
           Struggling to Choose? Discover Anime by Genre!
         </h1>
         <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 mt-3 md:mt-4 lg:mt-5">
-          {genreList.data.map((genre: any) => (
+          {genreList?.data.map((genre: any) => (
             <div
               key={genre.mal_id}
               className="text-neutral-50 text-[0.8rem] md:text-[0.9rem] lg:text-[1rem] break-words border border-violet-500 px-2 py-1 rounded-lg transition-all duration-300 ease-in-out hover:bg-violet-500 hover:scale-105 cursor-pointer"
