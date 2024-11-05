1009ce6ec9dd1347d05a38bd8e5a5ae76ecce37d
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-11-05 23:02:31 +0700

    add seo on seasonal page

--- src/app/(home)/seasonal/page.tsx
+++ src/app/(home)/seasonal/page.tsx
@@ -1,5 +1,13 @@
 import SeasonalAnimeContent from '@/components/seasonal/SeasonalAnimeContent';
 
+export const metadata = {
+  title: 'Seasonal Anime',
+  description: 'Must-Watch Anime of the Current Season',
+  openGraph: {
+    description: 'Must-Watch Anime of the Current Season',
+  },
+};
+
 export default function SeasonalAnimePage() {
   return (
     <>
